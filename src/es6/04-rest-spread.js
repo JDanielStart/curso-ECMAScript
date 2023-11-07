5d055841103b4deffe0e845b89e8326595ebcae6
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-07 12:41:48 +0000

    Ejemplos del operador de propagación spread

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -9,3 +9,21 @@ console.log(a, fruits[1]);
 let user = {userName: "Oscar", age: 34};
 let {userName, age} = user;
 console.log(userName, user.age);
+
+//Spread operator
+
+let person = {name: "oscar", age: 28};
+let country = "MX";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+//Rest
+
+function sum(num, ...values) {
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum(1, 1, 2, 3);
