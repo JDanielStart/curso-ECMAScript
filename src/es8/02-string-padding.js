268cf62f92b49dfcc9aa040a0bf45e88b56eec5a
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-10 11:15:31 +0000

    Ejemplo de String padding

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,6 @@
+/* Rellena la cadena con la cadena que le indiques
+hasta completar el número de caracteres que se indica */
+const string = "Hello";
+
+console.log(string.padStart(10, "_"));
+console.log(string.padEnd(10, "_"));
