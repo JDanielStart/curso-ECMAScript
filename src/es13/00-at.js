c730c95f69915c96c5711472e82c739ba42aea37
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-11-13 13:59:39 +0000

    Ejemplo de at para arreglos

---
+++ src/es13/00-at.js
@@ -0,0 +1,8 @@
+const array = ["one", "two", "three", "four", "five", "six"];
+
+console.log(array[array.length - 1]);
+
+//Con at los negativos son las posiciones del array al reves
+console.log(array.at(-1));
+console.log(array.at(-2));
+console.log(array.at(0));
